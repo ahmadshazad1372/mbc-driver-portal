deb5b549ca3dee5297199db1234020affa8c4944
Ahmad Shehzad Mansha <humsb77hain15@gmail.com>
2026-09-01 18:48:47 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mbc-driver-portal-v2';
+const CACHE_NAME = 'mbc-driver-portal-v3';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
